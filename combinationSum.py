6a1cff70c085d91faf5453cd744af312cd4952bb
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-02 22:22:39 -0400

    fixed bugs

--- combinationSum.py
+++ combinationSum.py
@@ -1,3 +1,4 @@
+import copy
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -9,19 +10,20 @@ class Solution(object):
         self.helper(candidates, target, result)
         return result
      
-    def helper(self, candidates, target, result, temp=[]):
+    def helper(self, candidates, target, result, temp=[], prev=float('-inf')):
 
         if target == 0:
             print(temp)
-
-            result.append(temp)
+            c = copy.deepcopy(temp)
+            result.append(c)
             return
         if target < 0:
             return
         for n in candidates:
-            if n >= target:
+            if n >= prev:
+                prev = n
                 temp.append(n)
-                self.helper(candidates, target - n, result, temp)
+                self.helper(candidates, target - n, result, temp, prev)
                 temp.pop()
 
             
